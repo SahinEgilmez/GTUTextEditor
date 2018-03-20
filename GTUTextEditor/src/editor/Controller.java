137e80ee76da64905cd68898d9a1537770ca7af7
Şahin Eğilmez <turkas64@gmail.com>
2018-03-20 22:57:26 +0300

    Created base project. Editor can open file and edit it.

---
+++ GTUTextEditor/src/editor/Controller.java
@@ -0,0 +1,67 @@
+package editor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+/**
+ * @author Sahin Egilmez
+ */
+public class Controller {
+
+    @FXML
+    private TextArea areaText;
+
+    private TextFile currentTextFile;
+
+    private Model model;
+
+    public Controller(Model model) {
+        this.model = model;
+
+    }
+
+    @FXML
+    private void onSave() {
+        TextFile textFile = new TextFile(currentTextFile.getFile(), Arrays.asList(areaText.getText().split("\n")));
+        model.save(textFile);
+
+    }
+
+    @FXML
+    private void onLoad() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File("./"));
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            IOResult<TextFile> io = model.load(file.toPath());
+
+            if (io.isOk() && io.hasData()) {
+                currentTextFile = io.getData();
+                areaText.clear();
+                currentTextFile.getContent().forEach(line -> areaText.appendText(line + "\n"));
+            } else {
+                System.out.println("Failed");
+            }
+        }
+    }
+
+    @FXML
+    private void onClose() {
+        model.close();
+    }
+
+    @FXML
+    private void onAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("About");
+        alert.setContentText("This is a simple text editor App");
+        alert.show();
+    }
+}
---
+++ GTUTextEditor/src/editor/TextFile.java
@@ -0,0 +1,27 @@
+package editor;
+
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * @author Sahin Egilmez
+ */
+public class TextFile {
+
+    private final Path file;
+
+    private final List<String> content;
+
+    public TextFile(Path file, List<String> content) {
+        this.file = file;
+        this.content = content;
+    }
+
+    public Path getFile() {
+        return file;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+}

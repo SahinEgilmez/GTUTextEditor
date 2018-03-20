8ed99fea122470ee0f4a76451ef42a6b0a2225b8
Şahin Eğilmez <turkas64@gmail.com>
2018-03-20 22:57:26 +0300

    Create base project. Editor can open file and edit it.

---
+++ GTUTextEditor/src/editor/App.java
@@ -0,0 +1,25 @@
+package editor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Sahin Egilmez
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui.fxml"));
+        loader.setControllerFactory(t -> new Controller(new Model()));
+
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
